ea22008753a2a05b37fef00835af4ad468ce243e
Stefan <131874499+stefanciu@users.noreply.github.com>
2023-06-05 21:33:30 +0100

    Added new lines in the file main.js

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,5 @@
-document.write("Hi, World!");
+var Sent1 = "This is the beginning of the string" // Defining variable Sent1 and giving it a string value
+var Sent2 = " and this is the end of the string"  // Defining variable Sent2 and giving it a string value
+
+document.write (Sent1 + Sent2)  // Concatinating the values of the variables Sent1 and Sent2
+window.alert("Stefan is here and moves the mouse") // Creating a window alert
