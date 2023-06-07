0de0f7f5a6acde5913a9bafb6dbe1a43a4852417
Stefan <131874499+stefanciu@users.noreply.github.com>
2023-06-07 11:46:03 +0100

    Function assignment

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+function My_First_Function (color, car) {
+    var car = "honda"  //Defining variable car and giving it a value
+    car += " civic" 
+    var color = "green" //Defining variable color and giving it a value
+    document.getElementById("Button_Text").innerHTML = color + " " + car;
+}
